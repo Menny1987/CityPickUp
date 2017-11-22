11ea0bd89d02ec54309ede6ede58489925720674
Menny1987 <Mennyyyy@gmail.com>
2017-11-23 00:31:00 +0200

    Setting up the project

---
+++ CityPickUp/CityPickUp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CityPickUp
+//
+//  Created by Menny Atia on 22/11/2017.
+//  Copyright © 2017 Menny Atia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+  
+
+}
+
